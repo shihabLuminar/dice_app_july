a13598f62d733147472aef3a5482657d8bbdb8f7
Shihab k s <161015991+shihabLuminar@users.noreply.github.com>
2024-10-07 16:48:40 +0530

    Update dice_screen.dart

--- lib/views/dice_screen/dice_screen.dart
+++ lib/views/dice_screen/dice_screen.dart
@@ -16,6 +16,16 @@ class _DiceScreenState extends State<DiceScreen> {
     "assets/images/d3.png",
     "assets/images/d4.png",
     "assets/images/d5.png",
+    "assets/images/d6.png", "assets/images/d1.png",
+    "assets/images/d2.png",
+    "assets/images/d3.png",
+    "assets/images/d4.png",
+    "assets/images/d5.png",
+    "assets/images/d6.png", "assets/images/d1.png",
+    "assets/images/d2.png",
+    "assets/images/d3.png",
+    "assets/images/d4.png",
+    "assets/images/d5.png",
     "assets/images/d6.png",
   ];
 
