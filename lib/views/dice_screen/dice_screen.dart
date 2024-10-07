eecbb0608c0e5e760454fecacd14f9653dfc01b1
Shihab k s <161015991+shihabLuminar@users.noreply.github.com>
2024-10-07 16:56:41 +0530

    Update dice_screen.dart

--- lib/views/dice_screen/dice_screen.dart
+++ lib/views/dice_screen/dice_screen.dart
@@ -17,6 +17,8 @@ class _DiceScreenState extends State<DiceScreen> {
     "assets/images/d4.png",
     "assets/images/d5.png",
     "assets/images/d6.png",
+     "assets/images/d5.png",
+    "assets/images/d6.png",
   ];
 
   int? randonNumber;
